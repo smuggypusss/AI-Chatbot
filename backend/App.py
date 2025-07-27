0beb3a8477a2f372e6e447095a1814f9186c9090
Aryan Kankheria <aryankankheria331@gmail.com>
2025-07-27 22:28:32 +0530

    Update CORS setting

--- backend/App.py
+++ backend/App.py
@@ -5,7 +5,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://ai-chatbot-v7w7-l0e3qcatq-smuggypusss-projects.vercel.app",
+        "http://localhost:5173"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
